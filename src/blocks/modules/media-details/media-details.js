687b86fa66f156ff2b63d41e2350ad0607fbf73d
Igor <50610898+Igor3007@users.noreply.github.com>
2022-08-17 22:54:53 +0300

    save page media

---
+++ src/blocks/modules/media-details/media-details.js
@@ -0,0 +1,13 @@
+modules.define('media-details', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
