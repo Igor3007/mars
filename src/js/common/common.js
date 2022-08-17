29fbe8f9d901e5f352cb2e7b3272ff2f4744fbfd
Igor <50610898+Igor3007@users.noreply.github.com>
2022-08-18 01:35:27 +0300

    save

--- src/js/common/common.js
+++ src/js/common/common.js
@@ -169,6 +169,37 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 
     }
+    /* ==============================================
+    datepicker
+    ============================================== */
+
+
+
+    if (document.querySelector('.top-filter__group')) {
+
+        (function () {
+            Datepicker.locales.ru = {
+                days: ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"],
+                daysShort: ["Вск", "Пнд", "Втр", "Срд", "Чтв", "Птн", "Суб"],
+                daysMin: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+                months: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
+                monthsShort: ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"],
+                today: "Сегодня",
+                clear: "Очистить",
+                format: "dd.mm.yyyy",
+                weekStart: 1,
+                monthsTitle: 'Месяцы'
+            }
+        })();
+
+        const elem = document.querySelector('.top-filter__group');
+        const input = elem.querySelector('input')
+        const rangepicker = new DateRangePicker(elem, {
+            language: 'ru'
+        });
+
+
+    }
 
 
 
