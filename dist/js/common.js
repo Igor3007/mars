67134b67802380e94bd668a185b41523fa611e95
Igor <50610898+Igor3007@users.noreply.github.com>
2022-08-20 16:31:12 +0300

    save page traning

--- dist/js/common.js
+++ dist/js/common.js
@@ -329,5 +329,23 @@ document.addEventListener("DOMContentLoaded", function (event) {
         document.querySelector('.school-conditions__title-nav').style.display = 'block'
     }
 
+    /* ==================================
+    faq
+    ==================================*/
+
+    if (document.querySelectorAll('.faq-item__question')) {
+
+        const faqItems = document.querySelectorAll('.faq-item__question')
+
+
+        faqItems.forEach(function (item, index) {
+            item.addEventListener('click', function () {
+                this.parentNode.classList.toggle('open')
+            })
+        })
+
+    }
+
+
 
 });
---
+++ src/blocks/modules/program-qualification/program-qualification.js
@@ -0,0 +1,13 @@
+modules.define('program-qualification', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
